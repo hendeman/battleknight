3c4f757720dda4030ceebf3e23f1e6c2f5ade06a
hendeman <designer_zh@mail.ru>
2024-10-07 21:27:55 +0300

    main

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+import argparse
+
 from module.all_function import save_file
 from module.operation import get_statistic_clan
-from setting import *
 from module.toplist import get_statistic, dict_values_difference, write_2dlist_to_excel
+from setting import FILE_NAME, STAT_FILE_NAME, STAT_FILE_LOSS
 
 if __name__ == "__main__":
-    if program_selection:
+    parser = argparse.ArgumentParser(description='Выбор программы и запись данных.')
+    parser.add_argument(
+        '--program_selection',
+        action='store_true',
+        help='Статистика сервера, по умолчанию стастика ордена'
+    )
+    parser.add_argument(
+        '--write_flag',
+        action='store_true',
+        help='Записать данные в файл'
+    )
+
+    args = parser.parse_args()
+
+    program_selection = args.program_selection
+    write_flag = args.write_flag
+
+    if not program_selection:
         all_dct = get_statistic_clan()
         if write_flag:
             print(f"Записать новые данные в {FILE_NAME}? [y/n]")
