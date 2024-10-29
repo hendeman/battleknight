c46a29276721adbcc982c226003df7fd31a2db33
hendeman <designer_zh@mail.ru>
2024-10-29 18:38:12 +0300

    вынес TG-токен в env

--- module/bot/config.py
+++ module/bot/config.py
@@ -1,6 +1,11 @@
-TOKEN = '7577099222:AAHlMBv2kXRjjXQOxfp8SNNyBL14ZF7_rAs'
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
 CHAT_ID = 808158849  # Ваш полученный chat_id
 LOG_FILE = 'logs/app.log'  # Путь к вашему лог-файлу
-# ALLOWED_USERS = {CHAT_ID}  # Множество разрешенных пользователей
 log_time = 20
 ALLOWED_USERS_FILE = 'allowed_users.txt'
