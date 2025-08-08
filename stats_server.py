91ade799fecd0ad17c29148fb47f94bedc384da6
hendeman <designer_zh@mail.ru>
2025-08-08 21:55:04 +0300

    Теперь в списке измененных имен не будет присутствовать текущее имя

--- stats_server.py
+++ stats_server.py
@@ -67,6 +67,8 @@ def union_list(name_file_1: str, name_file_2: str) -> list:
         if new_value != gamer_data[field_name]:
             if gamer_data[field_name] not in gamer_data[change_field]:
                 gamer_data[change_field].append(gamer_data[field_name])
+            if new_value in gamer_data[change_field]:
+                gamer_data[change_field].remove(new_value)
             p_log(f"{gamer_data['name']} изменил {field_name} на <{new_value}>")
             gamer_data[field_name] = new_value
             return gamer_data[change_field]
