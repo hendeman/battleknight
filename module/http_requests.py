ea03d7bd5adfe6563f91dab4d3f7a387457be99d
hendeman <designer_zh@mail.ru>
2025-08-08 22:01:45 +0300

    Добавлен функционал для работы с прокси

--- module/http_requests.py
+++ module/http_requests.py
@@ -3,14 +3,38 @@ from functools import wraps
 
 import requests
 from requests import Timeout, RequestException
+from requests.exceptions import ProxyError
 
 from logs.logs import p_log
 from module.all_function import get_random_value
 from module.data_pars import get_csrf_token, get_title
+from module.proxy.proxy_manager import create_proxy_manager, ProxyManager, proxies_validate
 from setting import cookies, headers
 
 csrf_token = None
 max_csrf_retries = 3
+
+
+class LazyProxyManager:
+    _instance = None
+    _custom_proxy = None
+
+    def __new__(cls, custom_proxy=None):
+        if custom_proxy:
+            # Создаем временный менеджер с кастомным прокси
+            return cls.create_custom_manager(custom_proxy)
+
+        if cls._instance is None:
+            cls._instance = create_proxy_manager() or None
+        return cls._instance
+
+    @classmethod
+    def create_custom_manager(cls, proxy):
+        """Создает временный менеджер с одним указанным прокси"""
+        validated = proxies_validate(proxy)
+        if not validated:
+            raise ValueError(f"Неверный формат прокси: {proxy}")
+        return ProxyManager(proxies=[proxy])
 
 
 def validate_status(response):
@@ -23,14 +47,37 @@ def validate_status(response):
 
 def request_error_handler(func):
     @wraps(func)
-    def wrapper(*args, **kwargs):
+    def wrapper(url, *args, **kwargs):
         timeout = kwargs.get('timeout', 10)
+        csrf = kwargs.get('csrf', False)
+        proxies = kwargs.get('proxies', None)
+        proxy_manager = kwargs.get('proxy_manage', LazyProxyManager())
+
+        if isinstance(proxy_manager, str):
+            proxy_manager = LazyProxyManager(custom_proxy=proxy_manager)
+
+        max_retries = 3
         csrf_retries = 0  # Счетчик попыток из-за CSRF
+        request_retries = 0
+
         while True:
             try:
-                response = func(*args, **kwargs)
-                # CSRF-логика только для make_request
+                if proxy_manager:
+                    current_proxy = proxy_manager.get_current_proxy()
+                    kwargs['proxies'] = {"http": current_proxy,
+                                         "https": current_proxy}
+                    proxies = kwargs.get('proxies').get('http')
+
+                response = func(url, *args, **kwargs)
+                validate_status(response)
+
                 if func.__name__ == 'make_request':
+                    p_log(f"GET {response.status_code}: {url} | proxy:{proxies}", level='debug')
+                else:
+                    p_log(f"POST {response.status_code}: {url} | proxy:{proxies}", level='debug')
+
+                # CSRF-логика только для make_request
+                if func.__name__ == 'make_request' and csrf:
                     global csrf_token
                     token = get_csrf_token(response)
 
@@ -47,20 +94,40 @@ def request_error_handler(func):
                             p_log('Достигнут лимит попыток из-за обновления CSRF токена', level='debug')
 
                 return response
+            except ProxyError as e:
+                request_retries += 1
+                old_proxy = proxy_manager.get_current_proxy()
+                p_log(f"ProxyError: {e}", level='debug')
+                p_log(f"Повтор через {timeout} сек...", level='debug')
+
+                if request_retries >= max_retries:
+                    new_proxy = proxy_manager.get_next_proxy()
+                    p_log(f"Превышено количество попыток для прокси {old_proxy}", level='debug')
+                    p_log(f"Будет выбран следующий прокси {new_proxy}", level='debug')
+                    request_retries = 0
+
             except Timeout as e:
                 p_log(f"Timeout: {e}", level='debug')
                 p_log(f"Повтор через {timeout} сек...", level='debug')
-                time.sleep(timeout)
+
             except RequestException as e:
                 p_log(f"Ошибка: {e}", level='debug')
                 p_log(f"Повтор через {timeout} сек...", level='debug')
-                time.sleep(timeout)
+
+            time.sleep(timeout)
 
     return wrapper
 
 
 @request_error_handler
-def make_request(url, timeout=10, game_sleep=True, browser_cookies=cookies, http_headers=headers, proxies=None):
+def make_request(url,
+                 timeout=10,
+                 game_sleep=True,
+                 browser_cookies=cookies,
+                 http_headers=headers,
+                 csrf=True,
+                 proxy_manage=None,
+                 proxies=None):
     response = requests.get(
         url,
         cookies=browser_cookies,
@@ -69,15 +136,20 @@ def make_request(url, timeout=10, game_sleep=True, browser_cookies=cookies, http
         timeout=timeout,
         proxies=proxies
     )
-    p_log(f"GET {response.status_code}: {url}", level='debug')
-    validate_status(response)
     if game_sleep:
         time.sleep(get_random_value(1, 2.5))
     return response
 
 
 @request_error_handler
-def post_request(url, data, timeout=10, csrf=True, browser_cookies=cookies, http_headers=headers, proxies=None):
+def post_request(url,
+                 data,
+                 timeout=10,
+                 csrf=True,
+                 browser_cookies=cookies,
+                 http_headers=headers,
+                 proxies=None,
+                 proxy_manage=None):
     """ Без csrf_token следующие POST-запросы:
         - создание группы, пас-группы;
         - получение списка баночек getPotionBar"""
@@ -93,6 +165,4 @@ def post_request(url, data, timeout=10, csrf=True, browser_cookies=cookies, http
         timeout=timeout,
         proxies=proxies
     )
-    p_log(f"POST {response.status_code}: {url}", level='debug')
-    validate_status(response)
     return response
