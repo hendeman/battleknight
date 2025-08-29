a6e699159b4c581adf910f41c7a5043a52ba2169
hendeman <designer_zh@mail.ru>
2025-08-29 07:54:03 +0300

    команда -cap теперь с параметром

--- module/war/cli.py
+++ module/war/cli.py
@@ -6,7 +6,7 @@ def war_parser():
     parser = argparse.ArgumentParser(description='Выбор программы и запись данных.')
 
     parser.add_argument('-sv', '--save', action='store_true', help='Напасть на нейтральный замок')
-    parser.add_argument('-cap', '--capture', action='store_true', help='Захватить вражеский замок')
+    parser.add_argument('-cap', '--capture', type=str, help='Захватить вражеский замок')
     parser.add_argument('-uc', '--clan', action='store_true', help='Обновить список клана')
     parser.add_argument('-r', '--read', action='store_true', help='Показать список игроков для удаления')
     parser.add_argument('-s', '--set', nargs='*', help='Установить/сбросить(reset) имена для удаления')
