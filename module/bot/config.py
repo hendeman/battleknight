d48521fbf34eb3e6031a0769e97e2b721c3f2b78
hendeman <designer_zh@mail.ru>
2025-09-13 13:47:53 +0300

    изменен путь лог

--- module/bot/config.py
+++ module/bot/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 CHAT_ID = 808158849  # Ваш полученный chat_id
-LOG_FILE_1 = 'logs/app.log'  # Путь к вашему лог-файлу
+LOG_FILE_1 = 'logs/app/app.log'  # Путь к вашему лог-файлу
 LOG_FILE_2 = 'logs/app_war.log'  # Путь к вашему лог-файлу
 log_time = 20
 ALLOWED_USERS_FILE = 'allowed_users.txt'
